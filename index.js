1f64bf56a1a2608730e6f0c0d5c8cde69dd8306d
JonnalagaddaNishitha <153357323+JonnalagaddaNishitha@users.noreply.github.com>
2023-12-09 15:16:06 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import CalculatorForm from '../components/CalculatorForm';
+import TollDetails from '../components/TollDetails';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Toll Calculator</h1>
+      <CalculatorForm />
+      <TollDetails />
+    </div>
+  );
+};
+
+export default Home;
